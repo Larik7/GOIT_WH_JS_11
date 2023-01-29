f68dfbfc90fe456578af65d6ab83fc4669b1078d
Larik7 <Larik777taganka@gmail.com>
2023-01-30 00:25:41 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import axios, {isCancel, AxiosError} from 'axios';
 import Notiflix from 'notiflix';
 // const axios = require('axios');
-
+// https://pixabay.com/api/
+// user_id:33216528
+// 33216528-23de23ca9469467d8b488f0af
 import axios from 'axios';
